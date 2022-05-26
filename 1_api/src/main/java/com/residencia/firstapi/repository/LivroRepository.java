4081a8e4b5f56ce3f1ecf006a38cc90e7aa5d36f
Talles Dias <tallesdelfino1@gmail.com>
2022-05-25 22:20:25 -0300

    Atividade1-Atualizada

--- 1_api/src/main/java/com/residencia/firstapi/repository/LivroRepository.java
+++
@@ -1,7 +0,0 @@
-package com.residencia.firstapi.repository;
-
-import com.residencia.firstapi.entity.Livro;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LivroRepository extends JpaRepository<Livro, Integer> {
-}
